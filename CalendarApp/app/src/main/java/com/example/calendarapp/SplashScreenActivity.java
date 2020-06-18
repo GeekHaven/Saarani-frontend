1aa0d4221fb7f2df650611fadb30be8423192af6
Varun Bhardwaj <iit2019091@iiita.ac.in>
2020-06-18 17:14:08 +0530

    First Commit

---
+++ CalendarApp/app/src/main/java/com/example/calendarapp/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.calendarapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
+
